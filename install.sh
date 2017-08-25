80ab4a8c1df69cd9cf0f479714fa81dad2442fe3
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:58:07 +0200

    bullet proof folder creation

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
-sudo wget -O http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /opt/
+sudo wget -N http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /opt/
 sudo chmod 644 /opt/jenkins.war
 export JENKINS_HOME=/jenkins
-sudo mkdir /jenkins
+sudo mkdir -p /jenkins
 sudo chmod +x ./install_jenkins_plugins.sh
-sudo mkdir /jenkins/plugins
+sudo mkdir -p /jenkins/plugins
 sudo ./install_jenkins_plugins.sh -d /jenkins/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
 sudo java -jar /opt/jenkins.war &
