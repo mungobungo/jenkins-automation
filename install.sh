720bc82115f24c08c33b0e987d45d1a24c1d0fca
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:08:29 +0200

    permission tweaks errors

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ sudo apt-get install unzip -y
 sudo apt-get install git -y
 sudo sh -c 'echo "127.0.0.1" > /tmp/ip.txt && paste /tmp/ip.txt /etc/hostname >> /etc/hosts'
 cd /tmp && git clone https://github.com/mungobungo/jenkins-automation.git && cd /tmp/jenkins-automation
+sudo chown -R ubuntu /tmp//jenkins-automation
 wget -O jenkins.war http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /home/ubuntu/
-chmod 666 /home/ubuntu/jenkins.war
-chown ubuntu /home/ubuntu/jenkins.war
+sudo chmod 666 /home/ubuntu/jenkins.war
+suod chown ubuntu /home/ubuntu/jenkins.war
 export JENKINS_HOME=/home/ubuntu/jenkins
 mkdir -p $JENKINS_HOME/plugins
 chmod +x ./install_jenkins_plugins.sh
