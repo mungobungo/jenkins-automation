f100dd4ee6c04f03b3cfbb4666356e7013ebb3d6
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 16:43:59 +0200

    fixing line endings again

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 sudo curl http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 pass=`sudo cat  /root/.jenkins/secrets/initialAdminPassword` 
-exec_jenkins = `java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/`
 echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("user1", "password123")' | java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =  
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin git
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin xunit
