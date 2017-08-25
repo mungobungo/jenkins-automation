beb433aaee1806452e62b7304ce789d61f109487
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:53:32 +0200

    another permission issue try

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
-sudo wget -O /opt/jenkins.war http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /opt/
-export JENKINS_HOME=/opt/jenkins
-mkdir -p /.jenkins/plugins/plugins
-chmod +x ./install_jenkins_plugins.sh
-chmod +x ./post_install.sh
-sudo ./install_jenkins_plugins.sh -d /.jenkins/plugins/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
+sudo wget http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /opt/
+sudo chmod 644 /opt/jenkins.war
+export JENKINS_HOME=/jenkins
+sudo mkdir /jenkins
+sudo chmod u=rwx ./install_jenkins_plugin.sh
+sudo mkdir /jenkins/plugins
+sudo ./install_jenkins_plugin.sh -d /jenkins/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
 sudo java -jar /opt/jenkins.war &
-sleep 60s && ./post_install.sh && echo "ready"
