cfdeeb44939e4c43a4ec4808b1352910a44a20e8
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:33:35 +0200

    added post-install script call

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo chown ubuntu /home/ubuntu/jenkins.war
 export JENKINS_HOME=/home/ubuntu/jenkins
 mkdir -p $JENKINS_HOME/plugins
 chmod +x ./install_jenkins_plugins.sh
+chmod +x ./post_install.sh
 sudo ./install_jenkins_plugins.sh -d $JENKINS_HOME/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
 sudo java -jar /home/ubuntu/jenkins.war &
-sleep 60
-echo "ready"
+sleep 60s && ./post_install.sh && echo "ready"
