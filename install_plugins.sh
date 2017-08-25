133b2f36d398cdf3884dd74ce6ac06eefc2d6a36
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 16:16:06 +0200

    user creation moved to install_plugins phase

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 sudo curl http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 pass=`sudo cat  /root/.jenkins/secrets/initialAdminPassword` 
+echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("user1", "password123")' | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy = \
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin git
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin xunit
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin amazon-ecs
