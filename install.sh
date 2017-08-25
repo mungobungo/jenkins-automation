b4da7ae2bc5afd64d245efe2ca86c8bce145f681
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:42:30 +0200

    fixing plugin installation issues

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -ex
 sudo wget -O /home/ubuntu/jenkins.war http://mirrors.jenkins-ci.org/war/2.75/jenkins.war -P /home/ubuntu/
-sudo chmod 666 /home/ubuntu/jenkins.war
-sudo chown ubuntu /home/ubuntu/jenkins.war
-export JENKINS_HOME=/home/ubuntu/jenkins
-mkdir -p $JENKINS_HOME/plugins
+export JENKINS_HOME=/opt/jenkins
+mkdir -p /.jenkins/plugins/plugins
 chmod +x ./install_jenkins_plugins.sh
 chmod +x ./post_install.sh
-sudo ./install_jenkins_plugins.sh -d $JENKINS_HOME/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
-sudo java -jar /home/ubuntu/jenkins.war &
+sudo ./install_jenkins_plugins.sh -d /.jenkins/plugins/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
+sudo java -jar /opt/jenkins.war &
 sleep 60s && ./post_install.sh && echo "ready"
--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
-curl http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar 
-pass=`sudo cat /jenkins/secrets/initialAdminPassword` 
-cat env.groovy | java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =  
+sudo curl http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar 
+pass=`sudo cat /root/.jenkins/secrets/initialAdminPassword` 
+cat env.groovy | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =  
 echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("admin", "password123")' | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy = 
 echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("user1", "password123")' | sudo java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy = 
 cat jenkins_git.groovy | java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ groovy =  
