d53d32a4ee5e376bf0850faad0fa4a8b31473ec2
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 14:05:46 +0200

    fixing type

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ chmod 666 /home/ubuntu/jenkins.war
 chown ubuntu /home/ubuntu/jenkins.war
 export JENKINS_HOME=/home/ubuntu/jenkins
 mkdir -p $JENKINS_HOME/plugins
-chmod +x ./install_jenkins_plugin.sh
-sudo ./install_jenkins_plugin.sh -d $JENKINS_HOME/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
+chmod +x ./install_jenkins_plugins.sh
+sudo ./install_jenkins_plugins.sh -d $JENKINS_HOME/plugins -a git@3.5.1 xunit@1.102 amazon-ecs@1.11 || error_exit 'Failed to install plugins'
 sudo java -jar /home/ubuntu/jenkins.war &
 sleep 60
 echo "ready"
