65c1ee69b60a8b17a24f8dd58920d97544090202
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 15:38:46 +0200

    post installation fixing

--- env.groovy
+++ env.groovy
@@ -5,9 +5,6 @@ Logger logger = Logger.getLogger("ecs-cluster")
 def env = System.getenv()
 //Print all the environment variables.
 
-env.each{
-logger.info(it)
-} 
 // You can also access the specific variable, say 'username', as show below 
 String user= env['USERNAME']
 
