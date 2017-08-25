92a28a1090e49c82abcb700122b87ac67f0a43db
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-25 16:14:30 +0200

    line enddings fixed

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 sudo curl http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
-pass=`sudo cat  /root/.jenkins/secrets/initialAdminPassword` \
+pass=`sudo cat  /root/.jenkins/secrets/initialAdminPassword` 
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin git
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin xunit
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ install-plugin amazon-ecs
