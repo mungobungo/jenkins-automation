1c82044ea004fffc258be8440ab48a5b3580ab4f
Oleg Taykalo <Oleg.Taykalo@gmail.com>
2017-08-28 17:14:33 +0200

    removed buggy env.groovy call

--- configure_plugins.sh
+++ configure_plugins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 wget -N --quiet http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
-sh ./call_jenkins_groovy.sh env.groovy
 sh ./call_jenkins_groovy.sh jenkins_git.groovy
 sh ./call_jenkins_groovy.sh setup_ecs.groovy
 echo 'DONE configuring plugins'
