53783bfe281ade6739d112c0598042c999c3ebb2
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-08-29 13:05:01 +0200

    used password retrieval via environment variable

--- call_jenkins_command.sh
+++ call_jenkins_command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 command=$1
-pass=`sudo cat /root/.jenkins/secrets/initialAdminPassword`
+pass=`sh ./get_default_jenkins_pass.sh`
 java -jar jenkins-cli.jar -auth admin:$pass -s http://localhost:8080/ $command
